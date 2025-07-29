3960da26fb59cdfe069f573e0964d3bf7523a9dd
regsah <uregilsahin@gmail.com>
2025-07-29 21:56:09 +0300

    Add RecorderPage

---
+++ apps/webapp/src/pages/RecorderPage.jsx
@@ -0,0 +1,12 @@
+import './RecorderPage.css'
+import TextInput from '../components/TextInput';
+
+function RecorderPage() {
+    return (
+        <div className="RecorderPage-container">
+            <TextInput />
+        </div>
+    );
+}
+
+export default RecorderPage
