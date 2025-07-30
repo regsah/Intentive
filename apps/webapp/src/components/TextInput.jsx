bf601b389e440bfde3cc1e9c3f9c98a9c801197b
regsah <uregilsahin@gmail.com>
2025-07-30 09:26:15 +0300

    ADD styling and pause button

--- apps/webapp/src/components/TextInput.jsx
+++ apps/webapp/src/components/TextInput.jsx
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 
 import { MdKeyboardVoice } from "react-icons/md";
 import { FaArrowUp } from "react-icons/fa";
+import { GiCancel } from "react-icons/gi";
+import { FaPause } from "react-icons/fa6";
+
+
 import './styles/TextInput.css'
 
-function TextInput() {
+function TextInput({isRecording, setIsRecording}) {
     function handleSubmit(query, setQuery) {
         console.log('Submitted query:', query);
         setQuery('');
@@ -22,9 +26,26 @@ function TextInput() {
                 rows={4}
             />
             <div className="TextInput-buttonbar">
-                <button className="TextInput-button TextInput-button-left"> <MdKeyboardVoice /> </button>
-                <button className="TextInput-button TextInput-button-right" 
-                        onClick={() => handleSubmit(query, setQuery)}>
+                {
+                    isRecording ? (
+                        <div className="TextInput-left-buttons-container">
+                            <button className="TextInput-button TextInput-button-left TextInput-button-isRecording" 
+                                    onClick={() => setIsRecording(false)}
+                                    style={{width: '40%', height: '80%'}}>
+                                <GiCancel />
+                            </button>
+                            <button className='TextInput-button TextInput-button-left TextInput-button-pauseRecording'
+                                    style={{width: '40%', height: '80%'}}>
+                                <FaPause />
+                            </button>
+                        </div>
+                    ) : (
+                        <button className="TextInput-button TextInput-button-left" onClick={() => setIsRecording(true)}>
+                            <MdKeyboardVoice />
+                        </button>
+                    )
+                }
+                <button className="TextInput-button TextInput-button-right" onClick={() => handleSubmit(query, setQuery)}>
                     <FaArrowUp />
                 </button>
             </div>
--- apps/webapp/src/pages/RecorderPage.jsx
+++ apps/webapp/src/pages/RecorderPage.jsx
@@ -1,10 +1,17 @@
+import React, { useState } from 'react';
+import { MdKeyboardVoice, MdWidthFull } from "react-icons/md";
 import './RecorderPage.css'
 import TextInput from '../components/TextInput';
 
 function RecorderPage() {
+    const [isRecording, setIsRecording] = useState(false);
+    
     return (
         <div className="RecorderPage-container">
-            <TextInput />
+            <div className={`RecorderPage-recording-symbol ${isRecording ? 'recording-active' : ''}`}>
+                <MdKeyboardVoice style={{height:'80%', width:'80%'}}/>
+            </div>
+            <TextInput isRecording={isRecording} setIsRecording={setIsRecording} />
         </div>
     );
 }
