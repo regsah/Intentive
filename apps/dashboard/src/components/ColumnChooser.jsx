99c766556940fb4be5eff05fbf4f7d16d121dd31
regsah <uregilsahin@gmail.com>
2025-08-08 09:18:01 +0300

    UPDATE prevent hiding all columns in ColumnChooser

--- apps/dashboard/src/components/ColumnChooser.jsx
+++ apps/dashboard/src/components/ColumnChooser.jsx
@@ -3,6 +3,18 @@ import './styles/ColumnChooser.css'
 function ColumnChooser({openStates, setOpenStates})
 {
     function toggleColumn(column) {
+        if (openStates[column]) {
+            let closedCount = 0;
+            let totalCount = 0;
+
+            for (const key in openStates) {
+                if (!openStates[key]) closedCount++;
+                totalCount++;
+            }
+
+            if (closedCount === totalCount - 1) return;
+        }
+
         setOpenStates({
             ...openStates,
             [column]: !openStates[column]
